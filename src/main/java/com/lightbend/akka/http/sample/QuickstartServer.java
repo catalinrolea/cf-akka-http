b54f233b39a753d008dbc3328f8e123dc3c70431
catalinrolea <kta_90_aug@yahoo.com>
2018-07-23 15:36:32 +0300

    akkaV3

---
+++ src/main/java/com/lightbend/akka/http/sample/QuickstartServer.java
@@ -0,0 +1,54 @@
+package com.lightbend.akka.http.sample;
+
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
+
+//#main-class
+public class QuickstartServer extends AllDirectives {
+
+    // set up ActorSystem and other dependencies here
+    public static void main(String[] args) throws Exception {
+        // boot up server using the route as defined below
+        ActorSystem system = ActorSystem.create("routes");
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        //In order to access all directives we need an instance where the routes are define.
+        QuickstartServer app = new QuickstartServer();
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
+                ConnectHttp.toHost("localhost", 8080), materializer);
+
+        System.out.println("Server online at http://localhost:8080\nPress RETURN to stop...");
+        System.in.read(); // let it run until user presses return
+
+        binding
+                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    }
+
+    //without path
+    private Route createRoute() {
+        return route(
+                        get(() ->
+                                complete("<P>Hello akka</P><h1>First deploy in cloud foundry</h1>")));
+    }
+}
+
+
+//#main-class
+
+
